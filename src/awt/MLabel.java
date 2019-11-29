cfb86102789cb2e1e2efed1118787564dc736afe
iwannaeat <1727642955@qq.com>
2019-11-29 17:05:06 +0800

    fileChooser and popMenu

--- src/awt/MLabel.java
+++ src/awt/MLabel.java
@@ -5,11 +5,13 @@ import view.*;
 import javax.naming.Context;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.text.AbstractDocument;
 import javax.swing.text.html.parser.ContentModel;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
 import java.util.Date;
 
 public class MLabel extends JLabel implements MouseListener {
@@ -29,8 +31,6 @@ public class MLabel extends JLabel implements MouseListener {
         this.addMouseListener(this);
         this.setFont(new Font (Font.DIALOG, Font.BOLD, 20));
     }
-
-
 
     @Override
     public void mouseClicked(java.awt.event.MouseEvent e) {
@@ -62,8 +62,10 @@ public class MLabel extends JLabel implements MouseListener {
                 MainView.mJpanel.updateUI();
                 System.out.println("点击了推荐");
                 break;
+            case "nList":
+                showFileAddDialog(MainView.mJpanel);
+                break;
         }
-
     }
 
     @Override
@@ -85,5 +87,36 @@ public class MLabel extends JLabel implements MouseListener {
     @Override
     public void mouseExited(java.awt.event.MouseEvent e) {
         this.setBackground(Color.white);
+    }
+
+    private static void showFileAddDialog(Component parent) {
+        // 创建一个默认的文件选取器
+        JFileChooser fileChooser = new JFileChooser();
+
+        // 设置默认显示的文件夹为当前文件夹
+        fileChooser.setCurrentDirectory(new File("."));
+
+        // 设置文件选择的模式（只选文件、只选文件夹、文件和文件均可选）
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        // 设置是否允许多选
+        fileChooser.setMultiSelectionEnabled(true);
+
+        // 添加可用的文件过滤器（FileNameExtensionFilter 的第一个参数是描述, 后面是需要过滤的文件扩展名 可变参数）
+        fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("zip(*.zip, *.rar)", "zip", "rar"));
+        // 设置默认使用的文件过滤器
+        fileChooser.setFileFilter(new FileNameExtensionFilter("image(*.jpg, *.png, *.gif)", "jpg", "png", "gif"));
+
+        // 打开文件选择框（线程将被阻塞, 直到选择框被关闭）
+        int result = fileChooser.showOpenDialog(parent);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            // 如果点击了"确定", 则获取选择的文件路径
+            File file = fileChooser.getSelectedFile();
+
+            // 如果允许选择多个文件, 则通过下面方法获取选择的所有文件
+            // File[] files = fileChooser.getSelectedFiles();
+
+            System.out.println("打开文件: " + file.getAbsolutePath() + "\n\n");
+        }
     }
 }
